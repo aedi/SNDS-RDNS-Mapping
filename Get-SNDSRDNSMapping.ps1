b2abb81a758dc58d71f095c2af266f22b1aceba8
aedi <andreas@it-helden.li>
2018-07-24 11:55:16 +0200

    initial

---
+++ Get-SNDSRDNSMapping.ps1
@@ -0,0 +1,14 @@
+﻿$key = "SNDS KEY GOES HERE"
+
+$content = invoke-webrequest https://sendersupport.olc.protection.outlook.com/snds/ipStatus.aspx?key=$key
+
+$IPs = $content.Content -split "`r`n"
+
+foreach($IP in $IPs) {
+    if($IP -ne "") {
+        $ipos = $IP.IndexOf(",")
+        $lIP = $IP.Substring(0, $ipos)
+        $rdns = (Resolve-DnsName $lIP).NameHost
+        Write-Host "$lIP - $rdns"
+    }
+}
